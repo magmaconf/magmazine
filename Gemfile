6d11896ad98b34f54893398c5130802adca0668e
Javier Velazquez <javier.velazquez@crowdint.com>
2011-11-04 12:48:44 -0600

    initial commit for cms integration

--- Gemfile
+++ Gemfile
@@ -1,4 +1,23 @@
-source "http://rubygems.org"
+source 'http://rubygems.org'
 
-gem 'rack-contrib'
-gem 'rack-rewrite'
+gem 'rails', '3.1.1'
+
+gem 'comfortable_mexican_sofa'
+gem 'json'
+gem 'jquery-rails'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magazine::Application.config.secret_token = 'ef0d9af1459c3d0de5bc9adcd0ce5bf2c4fab3ce23390c43b5ae4b8b53ce09845dc054a5d32ce379a07b33179363d0f317564bf87f032890018bcd9bad348fdf'
