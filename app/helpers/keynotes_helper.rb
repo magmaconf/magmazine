d6a6dc58cb2bc43a77d38c4b5811c2d49919ed8f
Ignacio Galindo <ignacio.galindo@crowdint.com>
2012-06-19 00:45:17 -0500

    Add keynote action icons (Style)

---
+++ app/helpers/keynotes_helper.rb
@@ -0,0 +1,9 @@
+module KeynotesHelper
+   def render_keynote_actions
+     action_buttons = ''
+     [:video, :slides, :gallery].each do |action|
+       action_buttons +=  content_tag(:span, '' ,class: 'keynote-action-icon' ,id: "keynote-#{action}")
+     end
+     action_buttons.html_safe
+   end
+end
