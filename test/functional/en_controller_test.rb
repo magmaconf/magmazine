d3a7c2b0ca8bb40900c41fc4fbbcd9ec74d89f91
Ignacio Galindo <ignacio.galindo@crowdint.com>
2012-06-19 00:35:54 -0500

    Remove unused tests

--- test/functional/en_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class EnControllerTest < ActionController::TestCase
-  test "should get editorial" do
-    get :editorial
-    assert_response :success
-  end
-
-end
--- test/functional/home_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class HomeControllerTest < ActionController::TestCase
-  test "should get magmazine" do
-    get :magmazine
-    assert_response :success
-  end
-
-end
