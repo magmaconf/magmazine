4b400d0a6546e6d3a2a19fcfac949039dac7cd70
emmanuel delgado <emmanuel.delgado@crowdint.com>
2012-07-03 12:24:06 -0500

    Display localizations from yaml files, first attempt

--- spec/integrations/content_admin_spec.rb
+++ spec/integrations/content_admin_spec.rb
@@ -15,11 +15,12 @@ describe "Content Admin" do
       fill_in 'user_email', with: 'test@magmazine.com'
       fill_in 'user_password', with: '123test'
       click_button 'Sign in'
+      save_and_open_page
 
-      click_link 'Editorial'
-      click_link 'Text'
-      click_link 'main_title'
-      fill_in 'main_title_content', with: 'The new main title'
+      click_link 'en.editorial.title1'
+      fill_in 'en.editorial.title1', with: 'The new main title'
+      click_button 'Update'
+      page.should have_content('en.editorial.title1 has been updated')
       visit page_path(page: :editorial)
       page.should have_content('The new main title')
     end
