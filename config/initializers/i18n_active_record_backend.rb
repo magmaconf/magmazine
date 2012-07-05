3fe343544e578ce492edb9084694598665c5d2c8
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-05 18:13:37 -0500

    Add fix to see if an specif table exists

--- config/initializers/i18n_active_record_backend.rb
+++ config/initializers/i18n_active_record_backend.rb
@@ -1,3 +1,5 @@
- require 'i18n/backend/active_record'
- I18n.backend = I18n::Backend::Chain.
-   new(I18n::Backend::ActiveRecord.new, I18n.backend)
+if ActiveRecord::Base.connection.table_exists? 'translations'
+  require 'i18n/backend/active_record'
+  I18n.backend = I18n::Backend::Chain.
+    new(I18n::Backend::ActiveRecord.new, I18n.backend)
+end
