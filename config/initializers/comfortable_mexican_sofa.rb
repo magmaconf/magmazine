8a6ae63715073a15981ab72521165d061aa4b2c5
Mario A Chavez <mario.chavez@gmail.com>
2013-01-03 20:01:23 -0600

    invert aliases

--- config/initializers/comfortable_mexican_sofa.rb
+++ config/initializers/comfortable_mexican_sofa.rb
@@ -98,7 +98,7 @@ ComfortableMexicanSofa.configure do |config|
   # production env with dev/testing envs.
   # e.g. config.site_aliases = {'host.com' => 'host.inv', 'host_a.com' => ['host.lvh.me', 'host.dev']}
   # Default is nil (not used)
-  config.hostname_aliases = { 'magmazine.magmarails.com' => 'magmazine.magmaconf.com' }
+  config.hostname_aliases = { 'magmazine.magmaconf.com' => 'magmazine.magmarails.com' }
 
 end
 
